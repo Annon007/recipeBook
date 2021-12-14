f73c66e77866707ba9dd5d02e8635e6b3a02da61
Annon007 <62867777+Annon007@users.noreply.github.com>
2021-12-15 01:48:44 +0600

    still debugging

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,5 +1,5 @@
 import View from './View.js';
-import icons from 'url../../img/icons.svg';
+import icons from '../../img/icons.svg';
 // import fractional from '../../../node_modules/fractional';
 import { Fraction } from 'fractional';
 import { state } from '../module.js';
