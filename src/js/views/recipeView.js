c875a4e735d3e1585283963aba2ef352bdca4236
Annon007 <62867777+Annon007@users.noreply.github.com>
2021-12-15 01:41:06 +0600

    debugging

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,7 +1,7 @@
 import View from './View.js';
-import icons from 'url:../../img/icons.svg';
+import icons from 'url../../img/icons.svg';
 // import fractional from '../../../node_modules/fractional';
-// import { Fraction } from 'fractional';
+import { Fraction } from 'fractional';
 import { state } from '../module.js';
 
 class RecipeView extends View {
@@ -102,7 +102,7 @@ class RecipeView extends View {
             <svg class="recipe__icon">
               <use href="${icons}#icon-check"></use>
             </svg>
-            <div class="recipe__quantity">${ing?.quantity ? ing?.quantity: ""}</div>
+            <div class="recipe__quantity">${ing?.quantity ? new Fraction(ing?.quantity ?? "").toString() : ""}</div>
 
             <div class="recipe__description">
               <span class="recipe__unit">${ing.unit ?? ''}</span>
